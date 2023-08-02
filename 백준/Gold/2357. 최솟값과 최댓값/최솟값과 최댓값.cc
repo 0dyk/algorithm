ddf71248ab74b000feb6ca2f5f80d9745b9f1e6a
zerodyk <90687858+0dyk@users.noreply.github.com>
2023-08-02 23:25:22 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 92 ms, Memory: 5540 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
@@ -1,57 +1,58 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
+#define MAX_N 100100
 using namespace std;
-typedef long long ll;
 
-int n, m, sz;
-vector<pair<ll, ll>> v;	// min, max
+int N, M;
+int arr[MAX_N];
+int maxTree[MAX_N * 4];
+int minTree[MAX_N * 4];
 
+int maxv(int a, int b) {
+	return (a > b) ? a : b;
+}
+int minv(int a, int b) {
+	return (a <  b) ? a : b;
+}
 
-void update(int pos, ll x) {
-	int idx = sz + pos - 1;
-	while (idx) {
-		v[idx].first = min(v[idx].first, x);
-		v[idx].second = max(v[idx].second, x);
-		idx /= 2;
+void makeSeg(int left, int right, int node) {
+	if (left == right) {
+		maxTree[node] = minTree[node] = arr[left];
+		return;
 	}
+	int mid = (left + right) >> 1;
+	makeSeg(left, mid, node << 1);
+	makeSeg(mid + 1, right, node << 1 | 1);
+	maxTree[node] = maxv(maxTree[node << 1], maxTree[node << 1 | 1]);
+	minTree[node] = minv(minTree[node << 1], minTree[node << 1 | 1]);
+	return;
 }
 
-ll min_search(int pos, int left, int right, int start, int end) {
-	if (left > end || right < start) return 1e10;
-	if (left <= start && end <= right) return v[pos].first;
-
-	int mid = (start + end) / 2;
-	return min(min_search(pos * 2, left, right, start, mid), min_search(pos * 2 + 1, left, right, mid + 1, end));
-}
-
-ll max_search(int pos, int left, int right, int start, int end) {
-	if (left > end || right < start) return -1;
-	if (left <= start && end <= right) return v[pos].second;
-
-	int mid = (start + end) / 2;
-	return max(max_search(pos * 2, left, right, start, mid), max_search(pos * 2 + 1, left, right, mid + 1, end));
+pair<int, int> findMinMAx(int node, int start, int end, int left, int right) {
+	if (left > end || right < start) {
+		return make_pair(INT32_MAX, 0);
+	}
+	else if (left <= start && end <= right) {
+		return make_pair(minTree[node], maxTree[node]);
+	}
+	else {
+		pair<int, int> l, r;
+		l = findMinMAx(node * 2, start, (start + end) / 2, left, right);
+		r = findMinMAx(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+		return make_pair(min(l.first, r.first), max(l.second, r.second));
+	}
 }
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m;
-	sz = 1;	while (sz < n) sz *= 2;
-	v.resize(sz * 2);
-	for (int i = 1; i < v.size(); i++) {
-		v[i] = {1e10, -1};
-	}
+	cin >> N >> M;
+	for (register int i = 1; i <= N; i++) cin >> arr[i];
+	makeSeg(1, N, 1);
 
-	/*init*/
-	for (int i = 1; i <= n; i++) {
-		ll x; cin >> x;
-		update(i, x);
-	}
-
-	for (int i = 0; i < m; i++) {
-		int x, y;
-		cin >> x >> y;
-		cout << min_search(1, x, y, 1, sz) << ' ' << max_search(1, x, y, 1, sz) << '\n';
+	for (register int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		pair<int, int> res = findMinMAx(1, 1, N, a, b);
+		cout << res.first << ' ' << res.second << '\n';
 	}
 
 	return 0;
