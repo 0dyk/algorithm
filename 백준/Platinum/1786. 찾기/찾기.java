b764742d58306d3a18f9c3c5da9a180c960ef5ef
zerodyk <90687858+0dyk@users.noreply.github.com>
2023-10-04 15:36:03 +0900

    [Platinum V] Title: 찾기, Time: 592 ms, Memory: 66696 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,70 +1,51 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
-	static char[] T;
-	static char[] P;
-	static int tLength;
-	static int PLength;
-	static List<Integer> list;
-	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		char[] T = in.readLine().toCharArray();
+		char[] P = in.readLine().toCharArray();
 		
-		list = new ArrayList<>();
+		int M = P.length;
+		int N = T.length;
 		
-		T = br.readLine().toCharArray();
-		P = br.readLine().toCharArray();
+		int[] pi = new int[M];
 		
-		tLength = T.length;
-		PLength = P.length;
-		
-		// 1단계 : 부분일치 테이블 작성
-		int[] pi = new int[PLength];
-		
-		// i : 접미사 인덱스
-		// j : 접두사 인덱스
-		
-		for(int i = 1, j = 0; i < PLength; i++) {
-			while(j > 0 && P[i] != P[j]) {
+		// 부분 일치 테이블 생성
+		for(int i=1, j=0; i<M; i++) {
+			while(j>0 && P[i]!=P[j]) {
 				j = pi[j-1];
 			}
 			
-			// 접미사와 접두사가 같은 경우, 그 길이를 부분일치 테이블에 기록
 			if(P[i] == P[j]) {
 				pi[i] = ++j;
-			}
-			else {
+			}else {
 				pi[i] = 0;
 			}
 		}
 		
-		// 2단계 : 부분일치 테이블을 활용하여 본문과 패턴 비교하기
+		// P에서 T 찾기
 		int cnt = 0;
+		List<Integer> list = new ArrayList<Integer>();
 		
-		// i : 본문 인덱스
-		// j : 패턴 인덱스
-		for(int i = 0, j = 0; i < tLength; i++) {
-			
-			// 본문과 패턴이 불일치하면
-			while(j > 0 && T[i] != P[j]) {
+		for(int i=0, j=0; i<N; i++) {
+			while(j>0 && T[i] != P[j]) {
 				j = pi[j-1];
 			}
 			
-			// 본문과 패턴의 한 글자가 일치하면
 			if(T[i] == P[j]) {
-				
-				// j가 패턴의 마지막 인덱스라면 패턴과 일치하는 단어찾기 완료
-				if(j == PLength-1) {
+				if(j == M-1) {
 					cnt++;
+					list.add(i-j+1);
 					
-					list.add(i - j + 1);
-					
-					// j 위치까지는 일치 했으므로 일치했던 곳의 인덱스 번호를 부분일치 테이블에서 가져옴
 					j = pi[j];
 				}else {
 					j++;
@@ -72,10 +53,11 @@ public class Main {
 			}
 		}
 		
-		System.out.println(cnt);
-		for(Integer i : list) {
-			System.out.print(i + " ");
+		out.write(cnt+"\n");
+		for(int n : list) {
+			out.write(n+" ");
 		}
 		
+		out.close();
 	}
 }
