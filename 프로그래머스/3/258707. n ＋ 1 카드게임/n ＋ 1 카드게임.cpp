d279c1319f6629a1ee049eb9938cf3b4ef92686c
zerodyk <90687858+0dyk@users.noreply.github.com>
2024-07-29 15:51:18 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
@@ -9,35 +9,39 @@ int checked[1000];
 priority_queue<int, vector<int>, greater<int>> pq;
 
 int solution(int coin, vector<int> cards) {
+    // 초기 라운드 1
     int answer = 1;
     
+    // 카드의 수
     int n = cards.size();
     
+    // 초기 카드로 가능한 경우 체크
     for(int i = 0; i < n / 3; ++i){
         checked[cards[i]] = 1;
         
         if(checked[n + 1 - cards[i]] != 0){
+            // 0의 비용으로 PASS
             pq.push(0);
-            // cout << cards[i] << " 와 " << n + 1 - cards[i] << " 초기 조합 \n";
         }
     }
     
+    // 라운드 진행
     for(int i = n / 3; i < n; ++i){
         checked[cards[i]] = 2;
         
         if(checked[n + 1 - cards[i]] != 0){
+            // checked 값에 따라서 1 or 2의 비용
             pq.push(checked[n + 1 - cards[i]]);
-            // cout << cards[i] << " 와 " << n + 1 - cards[i] << " 조합 " << checked[n + 1 - cards[i]] << "\n";
         }
         
+        // 2장 뽑을 때 마다
         if(i % 2 != 0){
-            cout << pq.size() << '\n';
-            
+            // 라운드 진행 불가
             if(pq.empty() || coin < pq.top()){
                 break;
             }
+            // 다음 라운드 진행
             else{
-                // cout << "코인 : " << coin << " - " << pq.top() << '\n';
                 coin -= pq.top();
                 pq.pop();
                 
@@ -46,15 +50,13 @@ int solution(int coin, vector<int> cards) {
         }
     }
     
-    while(!pq.empty() && pq.top() <= coin){
-        answer++;
-        coin -= pq.top();
-        pq.pop();
-    }
+//     while(!pq.empty() && pq.top() <= coin){
+//         answer++;
+//         coin -= pq.top();
+//         pq.pop();
+//     }
     
-    
-    
-    if(answer > n / 3 + 1) answer = n / 3 + 1;
+//     if(answer > n / 3 + 1) answer = n / 3 + 1;
     
     return answer;
 }
