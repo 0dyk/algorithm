f9473b25238986f8f96fff340e4c5684ba72646d
yeongdae <90687858+0dyk@users.noreply.github.com>
2025-06-14 07:35:33 +0900

    [Platinum V] Title: 정점들의 거리, Time: 392 ms, Memory: 53232 KB -BaekjoonHub

---
+++ 백준/Platinum/1761. 정점들의 거리/정점들의 거리.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static int N, M;
+    static int LOG;
+    static List<int[]>[] adjList;
+    static int[][] parent;
+    static int[] depth;
+    static int[] dist;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        adjList = new ArrayList[N + 1];
+        depth = new int[N + 1];
+        dist = new int[N + 1];
+
+        LOG = 0;
+        while ((1 << LOG) <= N) LOG++;
+
+        parent = new int[N + 1][LOG + 1];
+
+        for(int i = 0; i < N + 1; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < N - 1; i++) {
+            String[] str = br.readLine().split(" ");
+
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            int c = Integer.parseInt(str[2]);
+
+            adjList[a].add(new int[] {b, c});
+            adjList[b].add(new int[] {a, c});
+        }
+
+        makeTree(0, 1, 0);
+
+        for(int dep = 1; dep < LOG; dep++) {
+            for(int node = 1; node < N + 1; node++) {
+                parent[node][dep] = parent[parent[node][dep - 1]][dep - 1];
+            }
+        }
+
+        M = Integer.parseInt(br.readLine());
+        for(int i = 0; i < M; i++) {
+            String[] str = br.readLine().split(" ");
+
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+
+            sb.append(LCA(a, b)).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+
+    static int LCA(int oa, int ob) {
+        int a = oa;
+        int b = ob;
+
+        if(depth[a] > depth[b]) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        for(int i = LOG; i >= 0; i--) {
+            if(depth[b] - depth[a] >= (1 << i)) {
+                b = parent[b][i];
+            }
+        }
+
+        if(a == b) {
+            return dist[oa] + dist[ob] - dist[a] * 2;
+        }
+
+        for(int i = LOG; i >= 0; i--) {
+            if(parent[a][i] != parent[b][i]) {
+                a = parent[a][i];
+                b = parent[b][i];
+            }
+        }
+
+        return dist[oa] + dist[ob] - dist[parent[a][0]] * 2;
+    }
+
+    static void makeTree(int par, int node, int d) {
+        parent[node][0] = par;
+        depth[node] = depth[par] + 1;
+        dist[node] = d;
+
+        for(int i = 0; i < adjList[node].size(); i++) {
+            if(adjList[node].get(i)[0] == par) continue;
+
+            makeTree(node, adjList[node].get(i)[0], d + adjList[node].get(i)[1]);
+        }
+    }
+}
