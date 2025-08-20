894f3a293f61976985ab439db1e4997659f6bf0a
yeongdae <90687858+0dyk@users.noreply.github.com>
2025-08-20 15:17:56 +0900

    [level 3] Title: 순위, Time: 13.97 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,32 +1,38 @@
 class Solution {
-    public int solution(int n, int[][] results) {
-        int answer = n;
-
-        boolean winLose[][] = new boolean[101][101];
-        
-        for(int i = 0; i < results.length; ++i) {
-            winLose[results[i][0]][results[i][1]] = true;
-            // System.out.println(results[i][0] + " / " + results[i][1]);
-        }
     
-        for(int k = 1; k <= n; ++k) {
-            for(int i = 1; i<= n; ++i) {
-                for(int j = 1; j <= n; ++j) {
-                    if(winLose[i][k] && winLose[k][j]) {
-                        winLose[i][j] = true;
+    boolean[][] winlose;
+    
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+    
+        winlose = new boolean[n+1][n+1];
+        
+        for(int i = 0; i < results.length; i++) {
+                winlose[results[i][0]][results[i][1]] = true;
+        }
+        
+        // i -> k -> j
+        for(int k = 1; k <= n; k++) {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(winlose[i][k] && winlose[k][j]) {
+                        winlose[i][j] = true;
                     }
                 }
             }
         }
         
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= n; ++j) {
-                // i, j의 경기 결과를 알 수 없을 때
-                if (i != j && !winLose[i][j] && !winLose[j][i]) {
-                    answer--;
+        // answer
+        for(int i = 1; i <= n; i++) {
+            boolean flag = true;
+            for(int j = 1; j <= n; j++) {
+                if(i != j && !winlose[i][j] && !winlose[j][i]) {
+                    flag = false;
                     break;
                 }
             }
+            
+            if(flag) answer++;
         }
         
         return answer;
